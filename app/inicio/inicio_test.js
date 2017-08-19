c04fca64deedafeed4c54a1ee9e72d8ba16c1dc1
Manolo <manuelgmaroto@gmail.com>
2017-08-19 11:50:39 +0200

    Starting app

---
+++ app/inicio/inicio_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.inicio module', function() {
+
+  beforeEach(module('myApp.inicio'));
+
+  describe('view1 controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var view1Ctrl = $controller('InicioCtrl');
+      expect(view1Ctrl).toBeDefined();
+    }));
+
+  });
+});
